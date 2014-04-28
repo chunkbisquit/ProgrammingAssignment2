5b54b4682206427874c6137baa4fc58a0114ae4f
chunkbisquit <bradley.a@gmail.com>
2014-04-27 23:31:37 -0400

    added functionality to stubs
    updated cachematrix.R to have functions conforming to the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## these functions together can solve for the inverse of a matrix
+## and store the result. The "set" function in makeCacheMatrix
+## doesn't ever get used but is included for symmetry
 
-## Write a short comment describing this function
+## this function takes a matrix as its argument and creates and returns
+## a list of functions to get or set the matrix and to get or set
+## its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	m <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) m <<- inverse
+	getinverse <- function() m
 
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes a prepopulated makeCacheMatrix object as its argument
+## and checks for a cached inverse with getinverse. If none exists it solves
+## for the dataset's inverse and stores it with setinverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
